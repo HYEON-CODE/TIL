17f85040bf5121b2c5df451e9b24b68aa19e8362
Gwan Hyeon Lee <71334400+HYEON-CODE@users.noreply.github.com>
2024-03-14 15:28:58 +0900

    [이코테] chapter 4 구현 예제 2문제

---
+++ 2024/240314_TIL.py
@@ -0,0 +1,40 @@
+# 이코테 4-1.py
+
+# N을 입력받기
+n = int(input())
+x, y = 1, 1
+plans = input().split()
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_types = ['L', 'R', 'U', 'D']
+nx, ny = 0, 0
+
+# 이동 계획을 하나씩 확인
+for plan in plans:
+    # 이동 후 좌표 구하기
+    for i in range(len(move_types)):
+        if plan == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+        # 공간을 벗어나는 경우 무시
+        if nx < 1 or ny < 1 or nx > n or ny > n:
+            continue
+        # 이동 수행
+        x, y = nx, ny
+
+print(x, y)
+
+# 이코테 4-2.py
+
+# H를 입력받기
+h = int(input())
+
+count = 0
+for i in range(h+1):
+    for j in range(60):
+        for k in range(60):
+            # 매 시각 안에 '3'이 포함되어 있다면 카운트 증가
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+
+print(count)
