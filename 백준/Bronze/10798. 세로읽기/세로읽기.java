d8571fbe997f75ca9ef6064adfa18981936bd6d2
Gwan Hyeon Lee <71334400+HYEON-CODE@users.noreply.github.com>
2024-03-19 09:56:29 +0900

    [Bronze I] Title: 세로읽기, Time: 204 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String arr[] = new String[5];
+        
+        for(int i=0; i<5; i++){
+            arr[i] = sc.next();
+        }
+
+        for(int i=0; i<15; i++){
+            for(int j=0; j<5; j++){
+                if(arr[j].length()>i){
+                    System.out.print(arr[j].charAt(i));
+                }
+            }
+        }
+    }
+}
