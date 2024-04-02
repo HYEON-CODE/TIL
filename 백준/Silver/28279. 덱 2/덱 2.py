8a542ac1108a1f429cef3f4e3c29ff70c630c2b4
Gwan Hyeon Lee <71334400+HYEON-CODE@users.noreply.github.com>
2024-04-02 11:22:19 +0900

    [Silver IV] Title: 덱 2, Time: 1068 ms, Memory: 70664 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.py
+++ 백준/Silver/28279. 덱 2/덱 2.py
@@ -3,29 +3,40 @@ import sys
 
 deq = deque()
 n = int(sys.stdin.readline())
+size = 0
 for _ in range(n):
     command = sys.stdin.readline().rstrip()
     if len(command) > 1:
         if command[0] == '1':
             deq.appendleft(int(command[2:]))
+            size += 1
 
         else:
             deq.append(int(command[2:]))
+            size += 1
 
     elif command == '3':
-        print(-1 if len(deq)==0 else deq.popleft())
+        if size==0:
+            print(-1)
+        else:
+            print(deq.popleft())
+            size -= 1
 
     elif command == '4':
-        print(-1 if len(deq)==0 else deq.pop())
+        if size==0:
+            print(-1) 
+        else:
+            print(deq.pop())
+            size -= 1
 
     elif command == '5':
-        print(len(deq))
+        print(size)
 
     elif command == '6':
-        print(1 if len(deq)==0 else 0)
+        print(1 if size==0 else 0)
 
     elif command == '7':
-        print(-1 if len(deq)==0 else deq[0])
+        print(-1 if size==0 else deq[0])
 
     else:
-        print(-1 if len(deq)==0 else deq[-1])
+        print(-1 if size==0 else deq[-1])
