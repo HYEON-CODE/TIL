a4a1043212ce49dff81dc6a13cedddc06aede01b
Gwan Hyeon Lee <71334400+HYEON-CODE@users.noreply.github.com>
2024-07-15 17:15:09 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 360 ms, Memory: 317872 KB -BaekjoonHub

---
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -0,0 +1,53 @@
+import sys
+import heapq
+input = sys.stdin.readline
+sys.setrecursionlimit(150000)
+INF = int(1e9)
+
+def dijkstra(start):
+    distance = [INF] * (N+1)
+    h = []
+    distance[start] = 0
+    
+    heapq.heappush(h, (start, 0))
+
+    while h:
+        now,dist = heapq.heappop(h)
+
+        if dist > distance[now]:
+            continue
+        
+        for i in graph[now]:
+            cost = dist + i[1]
+
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(h, (i[0],cost))
+
+    return distance
+
+
+N, E = map(int,input().split())
+
+graph = [[] for _ in range(N+1)]
+
+for _ in range(E):
+    a,b,c = map(int,input().split())
+    graph[a].append((b,c))
+    graph[b].append((a,c))
+
+x,y = map(int,input().split())
+
+# 1에서 x로 x에서 y로 y에서 끝까지
+# 1에서 y로 y에서 x로 x에서 끝까지
+
+d1 = dijkstra(1)
+dx1 = dijkstra(x)
+dy1 = dijkstra(y)
+
+answer = min(d1[x] + dx1[y] + dy1[N], d1[y] + dy1[x] + dx1[N])
+
+if answer < INF:
+    print(answer)
+else:
+    print(-1)
